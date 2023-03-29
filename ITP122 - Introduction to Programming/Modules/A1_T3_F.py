023a4617e1f25513a6efe320698967489a5b6cad
Tia <101965297+smlcaffeineaddict@users.noreply.github.com>
2023-03-29 16:50:03 +1000

    Add

--- ITP122 - Introduction to Programming/Modules/A1_T3_F.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 12 13:06:26 2023
-
-@author: tiamegan
-"""
-
-print("\tBaby Baby,\n\n\t\t\t\tYes Mama,\n\n\tEating Sugar,\n\n\t\t\t\tNo Mama,\n\n\tTelling a lie,\n\n\t\t\t\tNo Mama,\n\n\tOpen your mouth,\n\n\t\t\t\tHa Ha Ha!")
